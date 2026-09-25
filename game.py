6fe7267955c967eae8385f0ccbcb781404cba36b
averyminskey-afk <avery.minskey@ascte.org>
2026-09-25 08:59:02 -0500

    ADDED HERO

--- game.py
+++ game.py
@@ -1,9 +1,15 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "The Iron Lung"
 
-
+def battle(hero: enemy,Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        hero_damage = hero.attack()
+        enemy.take_damage(hero_damage)
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
 def main():
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
@@ -11,14 +17,26 @@ def main():
     print("The gates are opening...")
 
     goblin = Goblin("Bubbles")
+    hero = hero("Bubbles the great")
 
-    print(f"{newgoblin.name} enters the arena with {goblin.health} health.")
+
+    print(f"{goblin.name} enters the arena with {goblin.health} health.")
+    print(f"{hero.name} enters the arena with {hero.health} health.")
 
     goblin = Goblin("Gribble")
 
-    print(f"{newgoblin.name} enters the arena with {goblin.health} health.")
+    print(f"{hero.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
 
+    print(f"{hero.name}) swings...")
+    heroDamage = hero.attack()
+    goblin.take_damage(heroDamage)
+
+    print(f"{goblin.name}) swings...")
+    goblinDamage = goblin.attack()
+    hero.take_damage(goblinDamage)
+
+    print("{goblin.name}")
 
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,27 @@
-class Hero:
-    """The hero blueprint will be implemented later in the project."""
+import random
 
-    pass
+class Hero:
+    """A playable character who battles enemies in the arena."""
+
+    def __init__(self, name):
+        self.name = name
+        self.health = 120
+        self.attack_power = 25
+
+    def attack(self):
+        """Return a random amount of damage."""
+        return random.randint(1, self.attack_power)
+    
+    def take_damage(self, damage):
+        """Reduce health without allowing it to fail below zero."""
+        self.health = self.health - damage
+        if self.health < 0:
+            self.health = 0
+            
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
+
+        def is_alive(self):
+            """Return True while the goblin has health remaining."""
+            return self.health > 0 
+
+        pass
